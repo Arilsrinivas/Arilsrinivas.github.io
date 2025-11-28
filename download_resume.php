c5aac35fad8ce34192aba640996c081073f96b78
Arilsrinivas <138370450+Arilsrinivas@users.noreply.github.com>
2025-11-28 08:08:00 +0530

    Add files via upload

---
+++ download_resume.php
@@ -0,0 +1,17 @@
+<?php
+$file = 'Aril_Srinivas_Resume_Updated.docx';
+
+if (file_exists($file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+    header('Content-Disposition: attachment; filename="'.basename($file).'"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    readfile($file);
+    exit;
+} else {
+    echo "File not found.";
+}
+?>
